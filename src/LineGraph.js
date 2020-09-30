6c5830e406ccdbbbf10ff8d5d1a5cbb8911b8d6f
CharlesWilmart <chwilmart@gmail.com>
2020-09-30 18:47:50 +0200

    installed numeral package + added options

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 const options = {
   legend: { display: false },
   elements: {
@@ -8,6 +9,38 @@ const options = {
     },
   },
   maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0"); // Have to install numeral package (npm i numeral)
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
 };
 function LineGraph() {
   const [data, setData] = useState({});
