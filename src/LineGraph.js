aa598b0c9d8513eb830c7358618370f13c3d6cbd
CharlesWilmart <chwilmart@gmail.com>
2020-09-30 19:02:50 +0200

    line chart working

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
+
 const options = {
   legend: { display: false },
   elements: {
@@ -42,51 +43,61 @@ const options = {
     ],
   },
 };
+
+const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+  // lastDataPoint is used to be able to only get the NEW cases per day
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
 function LineGraph() {
   const [data, setData] = useState({});
   //https://disease.sh/v3/covid-19/historical/all?lastdays=30
 
-  const buildChartData = (data, casesType = "cases") => {
-    const chartData = [];
-    let lastDataPoint;
-    // lastDataPoint is used to be able to only get the NEW cases per day
-    for (let date in data.cases) {
-      if (lastDataPoint) {
-        const newDataPoint = {
-          x: date,
-          y: data[casesType][date] - lastDataPoint,
-        };
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
-    }
-    return chartData;
-  };
-
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-      .then((response) => response.json())
-      .then((data) => {
-        //  console.log(data);
-        let chartData = buildChartData(data, "cases");
-        setData(chartData);
-      });
+    // Must have this piece of code to use async function in useEffect
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          //  console.log(data);
+          let chartData = buildChartData(data, "cases");
+          setData(chartData);
+        });
+    };
+    fetchData();
   }, []);
 
   return (
     <div>
-      <Line
-        data={{
-          datasets: [
-            {
-              backgroundColor: "rgba(204, 16, 52, 0.5",
-              borderColor: "#CC1034",
-              data: data,
-            },
-          ],
-        }}
-        options={options}
-      />
+      {/* check to see if data is populated */}
+      {/* ? means : check if data exist, could also be written :
+       {data && data.length > 0 && (*/}
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+          options={options}
+        />
+      )}
     </div>
   );
 }
